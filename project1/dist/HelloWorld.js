7ff952cd36edf32a61ab6708ab9baa1508aae463
BROWN-PROGRAMTECH <39649631+BROWN-PROGRAMTECH@users.noreply.github.com>
2023-06-10 15:11:47 +0100

    Initial commit

---
+++ project1/dist/HelloWorld.js
@@ -0,0 +1,4 @@
+"use strict";
+let Message = "Hello World";
+console.log(Message);
+//# sourceMappingURL=HelloWorld.js.map
---
+++ project1/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let Message = "Hello World";
+console.log(Message);
+//# sourceMappingURL=index.js.map
